9a37ccc4381be604471219b27a79dac04051de56
Stanley Sheeps <marthkot@gmail.com>
2026-01-20 15:41:20 +0500

    fix

--- modules/ui/containers.js
+++ modules/ui/containers.js
@@ -1,4 +1,4 @@
-import { createNavigationButtons } from "./buttons.js";
+//import { createNavigationButtons } from "./buttons.js";
 
 /**
  * Создаёт или возвращает существующий контейнер приложения
